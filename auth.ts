75b6e9d74cff0ea822c4b0309ebddb7803d9bb19
zhaopanpan <kangqiao610@gmail.com>
2024-08-09 16:31:07 +0800

    feat: auth

---
+++ auth.ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "./auth.config";
+import { z } from "zod";
+import { sql } from "@vercel/postgres"; // 这里需要注意！！！
+import type { User } from "@/app/lib/definitions";
+import bcrypt from "bcrypt";
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        console.log(`zp::: auth1`, credentials);
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        console.log(`zp::: auth2`, parsedCredentials);
+        if (parsedCredentials.success) {
+          const { email, password } = parsedCredentials.data;
+          const user = await getUser(email);
+
+          console.log(`zp::: auth3`, user);
+          if (!user) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          console.log(`zp::: auth4`, passwordsMatch);
+          if (passwordsMatch) return user;
+        }
+
+        console.log("Invalid credentials");
+        return null;
+      },
+    }),
+  ],
+});
